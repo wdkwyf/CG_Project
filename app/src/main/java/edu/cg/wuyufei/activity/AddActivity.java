9b1802ebaf03201220ec092a880b8f33b45c67c2
Yufei Wu <wuyufei1995@qq.com>
2015-11-17 22:05:32 +0800

    add TabLayout to card create

--- app/src/main/java/edu/cg/wuyufei/activity/AddActivity.java
+++ app/src/main/java/edu/cg/wuyufei/activity/AddActivity.java
@@ -3,10 +3,16 @@ package edu.cg.wuyufei.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
+import edu.cg.wuyufei.addForm.FragmentPagerAdapter;
 import edu.cg.wuyufei.gallery.R;
 
 public class AddActivity extends AppCompatActivity {
@@ -15,8 +21,15 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager(), AddActivity.this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
